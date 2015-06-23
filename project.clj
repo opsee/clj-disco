74b05c2ac32f6fdef3aeff28129108da26fadae9
Greg Poirier <grep@binary-snobbery.com>
2015-06-22 19:20:31 -0700

    Reorder project.clj

--- project.clj
+++ project.clj
@@ -2,13 +2,12 @@
   :description "A client library for registrator-backed service discovery"
   :url "https://github.com/opsee/clj-disco"
   :license "MIT"
-  
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [verschlimmbesserung "0.1.2"]
-                 [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories [["snapshots" {:url "s3p://opsee-maven-snapshots/snapshot"
                                :username :env
                                :passphrase :env
-                               :sign-releases false}]])
+                               :sign-releases false}]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [verschlimmbesserung "0.1.2"]
+                 [org.clojure/tools.logging "0.3.1"]])
