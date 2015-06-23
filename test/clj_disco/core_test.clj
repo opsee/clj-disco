586e876ad87287bb9845049eea2807970fae1dfd
Greg Poirier <grep@binary-snobbery.com>
2015-06-22 18:22:37 -0700

    Test multiple service endpoints endpoints

--- test/clj_disco/core_test.clj
+++ test/clj_disco/core_test.clj
@@ -9,4 +9,9 @@
       (disco/get-service-endpoint "test-service") => nil)
     (fact "get-service returns a service if one service entry"
       (disco/with-etcd (#'disco/etcd-create "/opsee.co/registrator/test-service/instance1" "foo:123"))
+      (disco/get-service-endpoint "test-service") => {:host "foo" :port "123"})
+    (fact "get-service returns a service if multiple entries"
+      (disco/with-etcd (#'disco/etcd-create "/opsee.co/registrator/test-service/instance1" "foo:123"))
+      (disco/with-etcd (#'disco/etcd-create "/opsee.co/registrator/test-service/instance2" "foo:123"))
+      (disco/with-etcd (#'disco/etcd-create "/opsee.co/registrator/test-service/instance3" "foo:123"))
       (disco/get-service-endpoint "test-service") => {:host "foo" :port "123"})))
